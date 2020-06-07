241c0bcf779d663d2ae73a5bfda5a2161e240499
T. Reepleks <treepleks@free.fr>
2020-06-07 19:11:32 +0200

    Preliminary trials at MQTT - Sonoff Tasmota activation

--- main/Eco-PV32.c
+++ main/Eco-PV32.c
@@ -70,6 +70,7 @@
 #include "nvs_flash.h"
 #include "lwip/err.h"
 #include "lwip/sys.h"
+#include "mqtt_client.h"
 
 #ifdef CONFIG_IDF_TARGET_ESP32
 #define CHIP_NAME "ESP32"
@@ -809,6 +810,72 @@ void sampling_isr_init()
 	lastzc_time = REG_READ(FRC_TIMER_COUNT_REG(1));
 	timer_start(TIMER_GROUP_1, TIMER_1);
 }
+
+//-------------------------------------------------------------
+//      MQTT
+// ------------------------------------------------------------
+static esp_err_t mqtt_event_handler_cb(esp_mqtt_event_handle_t event)
+{
+	esp_mqtt_client_handle_t client = event->client;
+	int msg_id;
+	// your_context_t *context = event->context;
+	switch (event->event_id)
+	{
+	case MQTT_EVENT_CONNECTED:
+		ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
+		msg_id = esp_mqtt_client_subscribe(client, "gBridge/u81/stat/SonoffAmpli/POWER", 0);
+		ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
+
+		msg_id = esp_mqtt_client_publish(client, "gBridge/u81/cmnd/SonoffAmpli/POWER", "ON", 0, 1, 0);
+		ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
+		break;
+	case MQTT_EVENT_DISCONNECTED:
+		ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
+		break;
+
+	case MQTT_EVENT_SUBSCRIBED:
+		ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
+		msg_id = esp_mqtt_client_publish(client, "/topic/qos0", "data", 0, 0, 0);
+		ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
+		break;
+	case MQTT_EVENT_UNSUBSCRIBED:
+		ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
+		break;
+	case MQTT_EVENT_PUBLISHED:
+		ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
+		break;
+	case MQTT_EVENT_DATA:
+		ESP_LOGI(TAG, "MQTT_EVENT_DATA");
+		printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
+		printf("DATA=%.*s\r\n", event->data_len, event->data);
+		break;
+	case MQTT_EVENT_ERROR:
+		ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
+		break;
+	default:
+		ESP_LOGI(TAG, "Other event id:%d", event->event_id);
+		break;
+	}
+	return ESP_OK;
+}
+
+static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
+{
+	ESP_LOGD(TAG, "Event dispatched from event loop base=%s, event_id=%d", base, event_id);
+	mqtt_event_handler_cb(event_data);
+}
+
+static void mqtt_app_start(void)
+{
+	esp_mqtt_client_config_t mqtt_cfg = {
+		.uri = CONFIG_BROKER_URL,
+	};
+
+	esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
+	esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, client);
+	esp_mqtt_client_start(client);
+}
+
 //-------------------------------------------------------------
 //      Main
 // ------------------------------------------------------------
@@ -865,6 +932,8 @@ void app_main(void)
 
 	// préparer le générateur d'impulsions pour le TRIAC/SSR
 	triac_controller_init();
+
+	mqtt_app_start();
 
 	for (;;)
 		vTaskDelay(50);
